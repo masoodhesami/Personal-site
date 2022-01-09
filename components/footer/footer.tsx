dc356bfb44070ffe9eed0f435eb375f81efd90b5
Masoud Hesami <masoodhesami79@gmail.com>
2022-01-09 12:36:07 +0330

    footer done :)

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from "./footer.module.css"
+
 const Footer = () => {
     return (
         <div className={styles.footerStyle}>
             <div className={styles.footerTextStyle}>
                 Design & Develop , Masoud Hesami
-                <span className={"pl-5"}> <span className={styles.AtomBlue}>Mail: </span>masoodhesami79@gmail.com </span>
+            </div>
+            <div className={styles.footerTextStyle}>
+                <span > <span className={styles.AtomBlue}>Mail: </span>
+                    masoodhesami79@gmail.com </span>
             </div>
         </div>
     );
