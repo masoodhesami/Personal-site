c7c852c055bd82d821bef94871450c3536c8ea56
Masoud Hesami <masoodhesami79@gmail.com>
2022-03-16 18:53:34 +0330

    modal added on section design

--- components/designs/designTemplates.tsx
+++ components/designs/designTemplates.tsx
@@ -1,38 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "./designs.module.css"
-import Image from 'next/image'
 import {ChevronDownIcon} from '@heroicons/react/outline'
+import Modal from "../modal/modal";
 
 const allDesigns = [
     {
         imageUrl: 'https://s23.picofile.com/file/8448278092/sepehr.png',
-        fullUrl: 'https://s23.picofile.com/file/8448277918/landing.png'
+        fullUrl: 'https://s23.picofile.com/file/8448277918/landing.png',
+        title: "Academy Design"
     },
     {
         imageUrl: 'https://s22.picofile.com/file/8448277976/Web_1920_%E2%80%93_1_2x.png',
-        fullUrl: 'https://s22.picofile.com/file/8448278692/shoes_ui.png'
+        fullUrl: 'https://s22.picofile.com/file/8448278692/shoes_ui.png',
+        title: "Shoes Website"
     },
     {
         imageUrl: 'https://s22.picofile.com/file/8448278076/rightel.png',
-        fullUrl: 'https://s23.picofile.com/file/8448278292/rightel_ui.png'
+        fullUrl: 'https://s23.picofile.com/file/8448278292/rightel_ui.png',
+        title: "Rightel Design"
     },
     {
         imageUrl: 'https://s22.picofile.com/file/8448278050/news.png',
-        fullUrl: 'https://s23.picofile.com/file/8448278542/news_ui.png'
+        fullUrl: 'https://s23.picofile.com/file/8448278542/news_ui.png',
+        title: "News Design"
     }
 ]
 const DesignTemplates = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [modalData, setModalData] = useState({title: "", fullUrl: "",});
+
+    const SetModal = (title: string, url: string) => {
+        setShowModal(true);
+        setModalData({title: title, fullUrl: url});
+    }
     return (
         <>
             <div className={styles.designTemplatesGrid}>
                 {allDesigns.map(design =>
                     <div key={design.imageUrl}>
-                        <a href={design.fullUrl} target={"_blank"}
-                           rel={"noreferrer"}>
-                            <img className={styles.designImages} src={`${design.imageUrl}`} alt="designs"/>
-                        </a>
+                        <img onClick={() => SetModal(design.title, design.fullUrl)} className={styles.designImages}
+                             src={`${design.imageUrl}`} alt="designs"/>
                     </div>
                 )}
+                {showModal && <Modal showModal={showModal}
+                                     modalData={modalData}
+                                     setShowModal={setShowModal}
+                />}
             </div>
             <div className={styles.divFlexCenter}>
                 <button id={'projects'} className={styles.moreBtn}>
---
+++ components/modal/modal.tsx
@@ -0,0 +1,87 @@
+import React, {Fragment, useRef, useState} from 'react'
+import {Dialog, Transition} from '@headlessui/react'
+import {DocumentIcon} from '@heroicons/react/outline'
+
+interface iProps {
+    showModal: Boolean,
+    setShowModal: React.Dispatch<React.SetStateAction<boolean>>
+    modalData: {
+        title: string,
+        fullUrl: string
+    }
+}
+
+const Modal: React.FC<iProps> = ({showModal, modalData, setShowModal}) => {
+    const cancelButtonRef = useRef(null)
+    return (
+        <Transition.Root show={Boolean(showModal)} as={Fragment}>
+            <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" initialFocus={cancelButtonRef}
+                    onClose={setShowModal}>
+                <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
+                    </Transition.Child>
+
+                    <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+            &#8203;
+          </span>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        enterTo="opacity-100 translate-y-0 sm:scale-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    >
+                        <div
+                            className="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                <div className="sm:flex sm:items-start">
+                                    <div
+                                        className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-purple-100 sm:mx-0 sm:h-10 sm:w-10">
+                                        <DocumentIcon className="h-6 w-6 text-purple-600" aria-hidden="true"/>
+                                    </div>
+                                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                                            {modalData.title}
+                                        </Dialog.Title>
+                                        <div className="mt-2">
+                                            <img src={modalData.fullUrl} alt="alt"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                                <button
+                                    type="button"
+                                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-purple-500 text-base font-medium text-white hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:ml-3 sm:w-auto sm:text-sm"
+                                    onClick={() => setShowModal(false)}
+                                >
+                                    seen
+                                </button>
+                                <button
+                                    type="button"
+                                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-purple-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                                    onClick={() => setShowModal(false)}
+                                    ref={cancelButtonRef}
+                                >
+                                    Cancel
+                                </button>
+                            </div>
+                        </div>
+                    </Transition.Child>
+                </div>
+            </Dialog>
+        </Transition.Root>
+    )
+}
+export default Modal;
